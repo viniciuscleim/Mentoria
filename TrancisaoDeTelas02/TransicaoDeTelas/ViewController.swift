bfe01276ca719a9e50d731effe325364d9123f65
ViniciusCleim <vinicius.cleim@gmail.com>
2022-10-14 10:42:37 -0300

    criei uma navigation controller e fiz transicao de tela para uma viewcontroller

--- TrancisaoDeTelas02/TransicaoDeTelas/ViewController.swift
+++ TrancisaoDeTelas02/TransicaoDeTelas/ViewController.swift
@@ -14,10 +14,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
     @IBAction func tappedButton(_ sender: UIButton) {
         
         performSegue(withIdentifier: "goTela02", sender: nil)
     }
+    
     
 }
 
