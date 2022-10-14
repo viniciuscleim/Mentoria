286469e02096d29e3924124a12594d988a4b43d2
ViniciusCleim <vinicius.cleim@gmail.com>
2022-10-14 10:45:01 -0300

    iniciando o projeto transicao de telas03

---
+++ TrancisaoDeTelas03/TransicaoDeTelas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransicaoDeTelas
+//
+//  Created by Vinicius Cleim on 14/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func tappedButton(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "goTela02", sender: nil)
+    }
+    
+    
+}
+
