3c35b8e0c646cc82cf19e01c6bdf18d311c1e79c
ViniciusCleim <vinicius.cleim@gmail.com>
2022-10-20 21:16:09 -0300

    primeiro push

---
+++ ProjetoTableView348/ProjetoTableView348/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProjetoTableView348
+//
+//  Created by Vinicius Cleim on 20/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configTableView()
+        // Do any additional setup after loading the view.
+    }
+    
+    func configTableView() {
+        
+        
+        
+    }
+
+}
+
